c464f7de1ad1567e147e32fe80802ab302d6db72
Aniket Dalvi <aniksd@amazon.com>
2026-07-10 12:07:24 -0700

    removed program object test

--- test/unit_tests/test_adapter_to_qiskit_result_pragmas.py
+++ test/unit_tests/test_adapter_to_qiskit_result_pragmas.py
@@ -3,8 +3,7 @@
 import pytest
 from qiskit import QuantumCircuit
 
-from braket.ir.jaqcd import AdjointGradient, Expectation, Probability, Variance
-from braket.ir.openqasm import Program
+from braket.ir.jaqcd import AdjointGradient, Expectation, Variance
 from qiskit_braket_provider import to_qiskit
 from qiskit_braket_provider.providers.qasm_context import _QiskitProgramContext
 
@@ -89,23 +88,6 @@ cnot q[0], q[1];
 """
     circuit = to_qiskit(source)
     assert circuit.metadata is None or "braket_result_pragmas" not in (circuit.metadata or {})
-
-
-def test_program_object_with_result_pragmas():
-    """to_qiskit should work with Program objects containing result pragmas."""
-    program = Program(
-        source="""OPENQASM 3.0;
-qubit[2] q;
-h q[0];
-cnot q[0], q[1];
-#pragma braket result probability q[0], q[1]
-"""
-    )
-    circuit = to_qiskit(program)
-    assert "braket_result_pragmas" in circuit.metadata
-    pragmas = circuit.metadata["braket_result_pragmas"]
-    assert len(pragmas) == 1
-    assert isinstance(pragmas[0], Probability)
 
 
 def test_result_pragma_does_not_add_measurements():
